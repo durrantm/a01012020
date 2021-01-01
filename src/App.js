28fb339176d979ec61ca30db5947da8df84eeeb8
Michael Durrant <m2@snap2web.com>
2021-01-01 13:50:38 -0500

    code

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
+function App() {
+  const [currentBTCprice, setCurrentBTCprice] = useState('');
+  const [calls, setCalls] = useState(0);
+
+  const getData = () => {
+    const itBitHost = 'https://api.itbit.com/v1/';
+    axios.get(itBitHost + 'markets/XBTUSD/ticker')
+      .then(function (response) {
+        const bid = Number(response.data.bid).toFixed(2);
+        setCurrentBTCprice(bid);
+        setCalls(calls + 1);
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+    const interval = setInterval(() => getData(), 2000)
+    return () => {
+      clearInterval(interval);
+    }
+  }, [currentBTCprice]);
+
+  return (
+    <div className="App">
+      <h1>PAXOS ATS</h1>
+      <h2>
+        {currentBTCprice.substring(0,2)},
+        {currentBTCprice.substring(2,8)}
+      </h2>
+      <div id="call-counter">
+        API call# {calls}<br /><br/>
+        {Date(Date.now()).substring(16, 24)}
+      </div>
+    </div>
+  );
+}
+export default App;
